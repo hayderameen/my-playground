c3c6ddde7c1881e16dcc88ca98c2dd27c6278165
Hayder <hayder204@gmail.com>
2020-05-13 07:57:31 +0500

    little animation

--- src/components/home/superPowers.jsx
+++ src/components/home/superPowers.jsx
@@ -6,7 +6,11 @@ import "../../css/home/superPowers.css";
 class SuperPowers extends Component {
   constructor(props) {
     super(props);
-    this.state = { showPowers: "hide-powers", flyhero: "" };
+    this.state = {
+      showPowers: "hide-powers",
+      flyhero: "",
+      showPowerIcon: "hide-power",
+    };
   }
 
   componentDidMount() {
@@ -18,52 +22,57 @@ class SuperPowers extends Component {
     if (scrollY >= 1080) {
       // My SuperPowers section is now showing
       // Lets display its animations here
-      this.setState({ showPowers: "show-powers", flyhero: "fly-hero-in" });
+      this.setState({
+        showPowers: "show-powers",
+        flyhero: "fly-hero-in",
+        showPowerIcon: "show-power",
+      });
     }
   };
 
   render() {
+    const { showPowers, flyhero, showPowerIcon } = this.state;
     return (
       <section id="superpowers-cover" onScroll={this.onScroll}>
-        <div id="superpower-heading" className={this.state.showPowers}>
+        <div id="superpower-heading" className={showPowers}>
           <div id="power-left"></div>
           <h1 id="power-text">MY SUPERPOWERS</h1>
           <div id="power-right"></div>
         </div>
-        <div id="superhero-figure" className={this.state.flyhero}></div>
+        <div id="superhero-figure" className={flyhero}></div>
         <div id="superPowers-texts">
           <div className="powers-row">
-            <div id="javascript">
+            <div id="javascript" className={showPowerIcon}>
               <div></div>
             </div>
-            <div id="html5">
+            <div id="html5" className={showPowerIcon}>
               <div></div>
             </div>
-            <div id="css">
-              <div></div>
-            </div>
-          </div>
-
-          <div className="powers-row">
-            <div id="webpack">
-              <div></div>
-            </div>
-            <div id="react">
-              <div></div>
-            </div>
-            <div id="node">
+            <div id="css" className={showPowerIcon}>
               <div></div>
             </div>
           </div>
 
           <div className="powers-row">
-            <div id="jquery">
+            <div id="webpack" className={showPowerIcon}>
               <div></div>
             </div>
-            <div id="sass">
+            <div id="react" className={showPowerIcon}>
               <div></div>
             </div>
-            <div id="socket">
+            <div id="node" className={showPowerIcon}>
+              <div></div>
+            </div>
+          </div>
+
+          <div className="powers-row">
+            <div id="jquery" className={showPowerIcon}>
+              <div></div>
+            </div>
+            <div id="sass" className={showPowerIcon}>
+              <div></div>
+            </div>
+            <div id="socket" className={showPowerIcon}>
               <div></div>
             </div>
           </div>
